48e339cc2f956637a6efa4ffb071c4efe4d8ca28
18211a12a2 <18211a12a2@bvrit.ac.in>
2024-06-20 05:11:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    const result = await response.json();
+    setData(result);
+  };
 
   return (
     <>
       <div className="container">
-        <button onClick={() => setVisible(!visible)}>
-          Toggle hide and show
-        </button>
-        
-        {visible && <h1>This is secret!!</h1>}
-      </div>
+        <h1>Website Link:</h1>
+        <span>https://official-joke-api.appspot.com/random_joke</span>
 
+        <button onClick={fetchData}>fetch data from link</button>
+
+        {data && (
+          <div className="result">
+            <h6>ID: {data.id}</h6>
+            <p>Setup: {data.setup}</p>
+            <p>Punchline: {data.punchline}</p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
